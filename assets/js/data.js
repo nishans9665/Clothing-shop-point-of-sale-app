6ef88dc716e30a9885640c9db1535f9f93aa1206
nishans9665 <your-mail.nishansameera@gmail.com>
2025-12-04 00:05:35 +0530

    grid product set

---
+++ assets/js/data.js
@@ -0,0 +1,48 @@
+// ------------------ Data ------------------ //
+
+const TAX_RATE = 0.08;
+
+let products = [
+  {
+    id: 1,
+    name: "Classic White Shirt",
+    category: "Men",
+    price: 2500,
+    image: "assets/img/shirt-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Slim Fit Jeans",
+    category: "Men",
+    price: 3500,
+    image: "assets/img/pants-1.jpg",
+  },
+  {
+    id: 3,
+    name: "Floral Summer Dress",
+    category: "Women",
+    price: 4200,
+    image: "assets/img/dress-1.jpg",
+  },
+  {
+    id: 4,
+    name: "Kids Graphic T-Shirt",
+    category: "Kids",
+    price: 1800,
+    image: "assets/img/shirt-1.jpg",
+  },
+  {
+    id: 5,
+    name: "Leather Handbag",
+    category: "Accessories",
+    price: 5200,
+    image: "assets/img/accessory-1.jpg",
+  },
+  {
+    id: 6,
+    name: "Casual Sneakers",
+    category: "Accessories",
+    price: 4800,
+    image: "assets/img/shoes-1.jpg",
+  },
+];
